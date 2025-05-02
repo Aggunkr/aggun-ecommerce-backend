ec50bb152bab80e5c61048537780981fcc682a4b
Aggunkr <aggunahmet2763@gmail.com>
2025-05-02 08:46:54 +0300

    fix: Linux uyumlu dosya adı düzeltildi (cartController)

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { addToCart, getCart, removeFromCart } = require("../controllers/cartController");
-const { protect } = require("../middleware/authMiddleware");
+const cartController = require("../controllers/cartController");
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post("/", protect, addToCart);
-router.get("/", protect, getCart);
-router.delete("/:productId", protect, removeFromCart);
+// Sepete ürün ekle
+router.post("/add", authMiddleware, cartController.addToCart);
+
+// Sepeti getir
+router.get("/", authMiddleware, cartController.getCart);
+
+// Sepetten ürün çıkar
+router.delete("/:productId", authMiddleware, cartController.removeFromCart);
 
 module.exports = router;
